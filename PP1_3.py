e8fb4d4cea926ac1e6e3c21d6bfa5b307fd3c88d
Johnny-Zhao-ICD2O1-01 <johnny.zhao@student.tdsb.on.ca>
2024-09-18 14:08:20 +0000

    commit

--- PP1_3.py
+++ PP1_3.py
@@ -1,16 +1,17 @@
-
-
 def q1():
-  #Write Assignment code here
-  #Remember your code must be indented once
+  print(" * * * * * * \n* * * * * * \n * * * * * * \n* * * * * * \n * * * * * * \n* * * * * * \n") 
+  print("* * * * * \n*       * \n*       * \n*       * \n* * * * * \n")
+  print("* * * * * * \n *       * \n  *     * \n   *   *    \n    * * \n     * \n")
+  #makes shapes in one line using the newline character
 
 def q2():
-  #Write Assignment code here
-  
-def q3():
-  #Write Assignment code here
-  
+  print("Hello", end = "")
+  print(" World")
+  #using end = "" to make the code not skip to the next line
 
+def q3():
+  print('"Man"\n"Board"\n"Man Overboard"')
+  #using single quotations so the code is be able to output double quotations
 
 #Do not alter anything after this line
 q1()
